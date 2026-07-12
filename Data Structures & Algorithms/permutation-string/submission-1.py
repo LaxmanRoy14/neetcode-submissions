e3cc5a167023851dc4f5fd3778af2e254542f1ba
Laxman Roy <113962144+LaxmanRoy14@users.noreply.github.com>
2026-07-12 23:37:02 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        # Frequency arrays for 26 lowercase English letters
+        s1_counts = [0] * 26
+        s2_counts = [0] * 26
+        
+        # Populate the initial frequencies for s1 and the first window of s2
+        for i in range(len(s1)):
+            s1_counts[ord(s1[i]) - ord('a')] += 1
+            s2_counts[ord(s2[i]) - ord('a')] += 1
+            
+        # If the first window is already a match
+        if s1_counts == s2_counts:
+            return True
+            
+        # Slide the window across s2
+        for i in range(len(s1), len(s2)):
+            # Add the incoming character to the right of the window
+            s2_counts[ord(s2[i]) - ord('a')] += 1
+            # Remove the outgoing character from the left of the window
+            s2_counts[ord(s2[i - len(s1)]) - ord('a')] -= 1
+            
+            # Check if the current window matches s1's frequency
+            if s1_counts == s2_counts:
+                return True
+                
+        return False
